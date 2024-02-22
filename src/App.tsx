b31729cbcf529f7d25025542311b9172a9e0c1fa
Neakhai Yauheni <beonezero@protonmail.com>
2024-02-22 21:24:23 +0300

    create StyledBtn, Box

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <StyledBtn>Button</StyledBtn>
     </div>
   );
 }
+
+const StyledBtn = styled.button`
+    background-color: #ff9c9c;
+    border: none;
+    padding: 10px 20px;
+    font-size: 22px;
+    border-radius: 5px;
+    color: snow;
+    font-weight: bold;
+`
+
+const Box = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    
+`
+
 
 export default App;
