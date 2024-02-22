0f88028f8bfd62f0f8056d8245f84f100a9706b7
Neakhai Yauheni <beonezero@protonmail.com>
2024-02-22 22:56:05 +0300

    add Button.styled.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,49 @@
 import React from 'react';
 import './App.css';
 import styled from "styled-components";
+import {StyledBtn} from "./components/Button.styled";
 
 function App() {
-  return (
-    <div className="App">
-        <StyledBtn>Button</StyledBtn>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Menu>
+                <ul>
+                    <li><a href="#1">menu item 1</a></li>
+                    <li><a href="#2">menu item 2</a></li>
+                    <li><a href="#3">menu item 3</a></li>
+                </ul>
+            </Menu>
+            <Box>
+                <StyledBtn>Button</StyledBtn>
+                <StyledBtn as={Link} href={"#"}>Button</StyledBtn>
+                <SuperButton>Super button</SuperButton>
+            </Box>
+        </div>
+    );
 }
 
-const StyledBtn = styled.button`
-    background-color: #ff9c9c;
-    border: none;
-    padding: 10px 20px;
-    font-size: 22px;
-    border-radius: 5px;
-    color: snow;
-    font-weight: bold;
+const SuperButton = styled(StyledBtn)`
+    background-color: greenyellow;
+`
+
+const Link = styled.a`
+    color: dodgerblue;
+`
+
+const Menu = styled.nav`
+    ul {
+        margin: 0;
+        padding: 0;
+        list-style: none;
+        display: flex;
+        li > a {
+            text-decoration: none;
+        }
+    }
+    li + li {
+        margin-right: 30px;
+    }
+
 `
 
 const Box = styled.div`
@@ -25,7 +51,14 @@ const Box = styled.div`
     justify-content: center;
     align-items: center;
     height: 100vh;
+    gap: 20px;
     
+    button {
+        cursor: pointer;
+    }
+    ${Link} {
+        cursor: zoom-in;
+    }
 `
 
 
---
+++ src/components/Button.styled.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const StyledBtn = styled.button`
+    background-color: #ff9c9c;
+    border: none;
+    padding: 10px 20px;
+    font-size: 22px;
+    border-radius: 5px;
+    color: snow;
+    font-weight: bold;
+
+    &:hover {
+        background-color: #d9b600;
+    }
+
+    &:last-child {
+        background-color: rgba(169, 99, 252, 0.5);
+    }
+`
